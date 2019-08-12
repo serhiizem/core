6586a97ff51a1f957aab4e95ade30d7d0ac6b666
serhiizem <serhiizem@gmail.com>
2019-08-12 21:45:10 +0300

    Fixing jackson dependency

--- data-structures/src/test/java/core/data_structures/TestData.java
+++ data-structures/src/test/java/core/data_structures/TestData.java
@@ -1,43 +1,8 @@
 package core.data_structures;
 
-public class TestData {
+class TestData {
 
     private static final int UNWEIGHTED = 1;
-
-    static class ArtificiallyUndirected {
-
-        static final WeightedDirectedGraph TEST_GRAPH_1 = new WeightedDirectedGraph();
-
-        static {
-            createFirstGraph();
-        }
-
-        private static void createFirstGraph() {
-            TEST_GRAPH_1.addVertex(0);
-            TEST_GRAPH_1.addVertex(1);
-            TEST_GRAPH_1.addVertex(2);
-            TEST_GRAPH_1.addVertex(3);
-            TEST_GRAPH_1.addVertex(4);
-            TEST_GRAPH_1.addVertex(5);
-
-            TEST_GRAPH_1.addEdge(0, 1, 4);
-            TEST_GRAPH_1.addEdge(1, 0, 4);
-            TEST_GRAPH_1.addEdge(0, 2, 3);
-            TEST_GRAPH_1.addEdge(2, 0, 3);
-            TEST_GRAPH_1.addEdge(1, 2, 5);
-            TEST_GRAPH_1.addEdge(2, 1, 5);
-            TEST_GRAPH_1.addEdge(1, 3, 2);
-            TEST_GRAPH_1.addEdge(3, 1, 2);
-            TEST_GRAPH_1.addEdge(3, 4, 2);
-            TEST_GRAPH_1.addEdge(4, 3, 2);
-            TEST_GRAPH_1.addEdge(4, 1, 4);
-            TEST_GRAPH_1.addEdge(1, 4, 4);
-            TEST_GRAPH_1.addEdge(4, 5, 6);
-            TEST_GRAPH_1.addEdge(5, 4, 6);
-            TEST_GRAPH_1.addEdge(4, 0, 4);
-            TEST_GRAPH_1.addEdge(0, 4, 4);
-        }
-    }
 
     static class WithNoCycles {
 
