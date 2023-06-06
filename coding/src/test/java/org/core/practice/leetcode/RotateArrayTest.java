80ce90a59139efb3109f223422c57a1fe388419f
Serhii Semlianyi <serhiizem@gmail.com>
2023-06-06 21:15:30 +0300

    additional testcase for rotate array

--- coding/src/test/java/org/core/practice/leetcode/RotateArrayTest.java
+++ coding/src/test/java/org/core/practice/leetcode/RotateArrayTest.java
@@ -1,6 +1,28 @@
 package org.core.practice.leetcode;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RotateArrayTest {
 
+    @Test
+    void test1() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        int step = 3;
 
+        int[] result = RotateArray.rotate(input, step);
+
+        assertThat(result).isEqualTo(new int[]{5, 6, 7, 1, 2, 3, 4});
+    }
+
+    @Test
+    void test2() {
+        int[] input = {-1, -100, 3, 99};
+        int step = 2;
+
+        int[] result = RotateArray.rotate(input, step);
+
+        assertThat(result).isEqualTo(new int[]{3, 99, -1, -100});
+    }
 }
