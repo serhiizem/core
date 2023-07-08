4d50ca4f0bd20641efededc972a342f2e61383f1
Serhii Semlianyi <serhiizem@gmail.com>
2023-07-08 22:36:53 +0300

    elements in reverse with for loop

--- ocp/src/test/java/org/core/practice/making_decisions/ForLoopTest.java
+++ ocp/src/test/java/org/core/practice/making_decisions/ForLoopTest.java
@@ -3,6 +3,9 @@ package org.core.practice.making_decisions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("StatementWithEmptyBody")
@@ -18,5 +21,18 @@ public class ForLoopTest {
         }
 
         assertThat(i).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("Create a for loop which produces 4 3 2 1 0")
+    void backwardsForLoop() {
+        List<Integer> results = new ArrayList<>();
+
+        int counter = 4;
+        for (int i = counter; i >= 0; i--) {
+            results.add(i);
+        }
+
+        assertThat(results).containsExactly(4, 3, 2, 1, 0);
     }
 }
